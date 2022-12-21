d7168eafd92d7535874b33bd81507228bb7bb25d
Artefius <kostikixum@gmail.com>
2022-12-21 23:12:52 +0700

    Domaska8

---
+++ DomashkaC#8/Zadacha54/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+void Insert(bool isRow, int dim, int[] source, int[,] dest)
+{
+    for (int k = 0; k < source.Length; k++)
+    {
+        if (isRow)
+            dest[dim, k] = source[k];//для строк 
+        else
+            dest[k, dim] = source[k];//для столбцов
+    }
+}//метод замены в стоке или столбце исходного массива на отсортированные значения.
+int[,] GenerateArray(int t, int i)
+{
+    int[,] table = new int[t, i];
+    Random random = new Random();
+    for (int a = 0; a < t; a++)
+    {
+        for (int b = 0; b < i; b++)
+        {
+            table[a, b] = random.Next(0, 9);
+        }
+    }
+    return table;
+}//метод генерации рандомного масива
+void PrintArray(int[,] array)
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            Console.Write(array[a, b] + " ");
+        }
+        Console.WriteLine();
+    }
+}//метод вывода масива
+void BubbleSort(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+        for (int j = 0; j < inArray.Length - i - 1; j++)
+        {
+            if (inArray[j] > inArray[j + 1])
+            {
+                int temp = inArray[j];
+                inArray[j] = inArray[j + 1];
+                inArray[j + 1] = temp;
+            }
+        }
+}//метод сортировка пузырьком через перевод в одномерный масив
+Console.WriteLine("Введите количество строк двумерного массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int colCount = m;
+int rowCount = n;
+int[,] arr = GenerateArray(rowCount, colCount);
+Console.WriteLine("Исходный массив");
+PrintArray(arr);
+Console.WriteLine("Сортировка по строкам: ");
+int[] row = new int[colCount];
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+            row[j] = arr[i, j];
+        BubbleSort(row);
+        Insert(true, i, row, arr);
+    }
+    PrintArray(arr);
---
+++ DomashkaC#8/Zadacha60/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся (HARD - случайных уникальных) двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int[] RandomIncluz(int[] array)
+{
+    int t = array.GetLength(0);
+    int[] table = new int[t];
+    Random random = new Random();
+    table[0] = random.Next(10, 99);
+    for (int a = 1; a < t; a++)
+    {
+         table[a] = random.Next(10,99);
+        for (int b = 0; b < a; b++)
+        {
+            if (table[b] == table[b + 1])
+                table[b] = random.Next(10, 99);
+        }
+    }
+    return table;
+}//генерация одномерного маисва с уникальными рандомными числами
+void PrintArray3(int[,,] array) //вывод трехмерного массива
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            for (int k = 0; k < array.GetLength(2); ++k)
+            {
+                Console.Write($"{array[a, b, k ]} ({a},{b},{k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}//метод вывода масива
+int[] arr1 = new int[8];
+int[,,] arr3 = new int[2, 2, 2];
+int[] arr2 = RandomIncluz(arr1);
+int t = 0;
+Console.WriteLine();
+for (int a = 0; a < arr3.GetLength(0); a++)
+{
+    for (int b = 0; b < arr3.GetLength(1); b++)
+    {
+        for (int k = 0; k < arr3.GetLength(2); ++k)
+        {
+            arr3[a, b, k] = arr2[t];
+            t++;
+        }
+    }
+}// заполняем масив из уникального набора чисел
+PrintArray3(arr3);
+       
+            
