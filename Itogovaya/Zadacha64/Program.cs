6f2e07c18b14f579759f5cf4254035872d859684
Artefius <kostikixum@gmail.com>
2022-12-22 22:47:47 +0700

    itogovaya

---
+++ Itogovaya/Zadacha64/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 64: Задайте значение N.
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+string Natur(int n)
+{
+    if (n == 1)
+    {
+        return "1";
+    }
+    return Natur(n - 1) + " " + n;
+}
+Console.WriteLine($"{Natur(N)}"); ; 
