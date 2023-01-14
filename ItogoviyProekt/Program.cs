37b46cc1fd769b62d1d1490ffc81ef9db4357b2a
Artefius <kostikixum@gmail.com>
2023-01-14 14:29:37 +0700

    добавлено описание работы алгоритма в фаиле README

---
+++ ItogoviyProekt/Program.cs
@@ -0,0 +1,38 @@
+﻿void PrintArray(int[] array)
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+
+
+        Console.Write(array[a] + " ");
+    }
+    Console.WriteLine();
+}//метод вывода масива
+int m = 3;
+string[] array = { "О", "да", "это",
+     "работает", "не", "гуд", };
+int index;
+if (!int.TryParse(Console.ReadLine(), out index))
+{
+    Console.WriteLine("Некорректный индекс");
+    return;
+}
+
+if (index < 0 || index >= array.Length)
+{
+    Console.WriteLine("Индекс за пределами массива");
+    return;
+}
+int[] newArray = new string[array.Length - 1];
+for (int i = 0; i < array.Length; i++)
+{
+    if (i < index)
+    {
+        newArray[i] = array[i];
+    }
+    else if (i > index)
+    {
+        newArray[i - 1] = array[i];
+    }
+}
+PrintArray(newArray);
